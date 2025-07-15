96d5cc486bec560ec372aff63a5fff40456cb0ae
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-15 19:24:55 +0200

    learn generic types

--- TypeScript/typescript/src/tutorial.ts
+++ TypeScript/typescript/src/tutorial.ts
@@ -542,3 +542,15 @@ basically an allrounder function
 function genericFunction<T>(arg: T): T {
   return arg
 }
+
+function generateStringArray(length: number, value:string): string[] {
+  let result: string[] = [];
+  result = Array(length).fill(value);
+  return result
+}
+
+function createArray<T>(length: number, value: T): Array<T> {
+  let result: T[] = [];
+  result = Array(length).fill(value);
+  return result
+}
