b6bc0831059b12682d852dc27b0ad44a4cc5da50
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-15 19:53:17 +0200

    finish challenge 18

--- TypeScript/typescript/src/tutorial.ts
+++ TypeScript/typescript/src/tutorial.ts
@@ -574,4 +574,40 @@ export interface AxiosResponse<T = any, D = any> {
   config: InternalAxiosRequestConfig<D>;
   request?: any;
 }
+
+// Fetch data
+// challenge#18
+const url = 'https://www.scourse-api.com/react-tours-project';
+
+type Tour = {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+}
+
+async function fetchData(url: string): Promise<Tour[]> {
+  try {
+    const response = await fetch(url)
+    if(!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data: Tour[] = await response.json()
+    console.log(data)
+  } catch (error) {
+    const errMsg = 
+      error instanceof Error ? error.message : 
+      'there was an error...'
+
+    console.error(errMsg)
+    return []
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});
 */
