4ffe2d7e5c802f32cb4053ddead4288ba240129b
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-17 23:55:11 +0200

    create basic tasks app

--- TypeScript/typescript/src/tasks.ts
+++ TypeScript/typescript/src/tasks.ts
@@ -1,3 +1,42 @@
 // 6:09:20
-const btn = document.querySelector<HTMLButtonElement>('.test-btn')!;
-btn.disabled
+const taskForm = document.querySelector<HTMLFormElement>('.form');
+const formInput = document.querySelector<HTMLInputElement>('.form-input');
+const taskListElement = document.querySelector<HTMLUListElement>('.list');
+
+type Task = {
+  description:string;
+  isCompleted: boolean;
+}
+
+const tasks:Task[] = [];
+
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const taskDescription = formInput?.value
+  if(taskDescription) {
+    const task:Task = {
+        description: taskDescription,
+        isCompleted: false
+    }
+    addTask(task)
+    // add task to list
+    // render task
+    renderTask(task)
+    // update local storage
+    formInput.value = ''
+    return;
+  }
+  alert('Please enter task description')
+})
+
+function addTask(task:Task):void{
+  tasks.push(task)
+  console.log(tasks)
+}
+
+function renderTask(task:Task):void {
+  const taskElement = document.createElement('li')
+  taskElement.textContent = task.description
+  taskListElement?.appendChild(taskElement)
+}
