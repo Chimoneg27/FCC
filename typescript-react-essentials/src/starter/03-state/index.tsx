8356b755e8afc8e0c2fb4e86d070838aecce0530
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-18 20:38:01 +0200

    learn how to use state with reactTS

--- typescript-react-essentials/src/starter/03-state/index.tsx
+++ typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
+type Link = {
+  id: number;
+  url: string;
+  text: string;
+}
+
+const navLinks: Link[] = [
+  {
+    id: 1,
+    url: 'somestring',
+    text: 'some text'
+  },
+    {
+    id: 2,
+    url: 'somestring',
+    text: 'some text'
+  },
+    {
+    id: 3,
+    url: 'somestring',
+    text: 'some text'
+  }
+]
+
 function Component() {
+  const [text, setText] = useState('shakeAndBake')
+  const [number, setNumber] = useState(1)
+  const [list, setList] = useState<string[]>([])
+  const [links, setLinks] = useState<Link[]>(navLinks)
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button className="btn btn-center" onClick={()=>{
+        setText('typeSctipt')
+        setNumber(0)
+        setList(['hello', 'world'])
+        setLinks([...links, {id:4, url:'hello', text:'hello'}])
+      }}>click me</button>
+      {/* <h2>State</h2> */}
     </div>
   );
 }
