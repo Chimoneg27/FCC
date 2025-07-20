aa3102cfb8c1fe36cfb95bc9aaa0724a36401262
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-20 21:33:22 +0200

    learn context hook with reactTS pt2

--- typescript-react-essentials/src/starter/06-context/context.tsx
+++ typescript-react-essentials/src/starter/06-context/context.tsx
@@ -1,17 +1,35 @@
 import { createContext, useContext, useState } from "react";
+import { set } from "zod";
 
-const ThemeProviderContext = createContext<string | undefined>(undefined)
+type Theme = "light" | "dark" | "system";
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+};
 
+const ThemeProviderContext = createContext<ThemeProviderState | undefined>(
+  undefined
+);
 
-export function ThemeProvider({children}:{children:React.ReactNode}){
-  return <ThemeProviderContext.Provider value='hello'>
-    {children}
-  </ThemeProviderContext.Provider>
+export function ThemeProvider({
+  children,
+  defaultTheme = "system",
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+  return (
+    <ThemeProviderContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
 }
 
 export const useTheme = () => {
-  const context = useContext(ThemeProviderContext)
-  if(context === undefined)
-    throw new Error('useTheme must be used within the ThemeProvider')
-  return context
-}
+  const context = useContext(ThemeProviderContext);
+  if (context === undefined)
+    throw new Error("useTheme must be used within the ThemeProvider");
+  return context;
+};
