069620b4f81f139191adcc08d3e4a1e7276c6323
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-20 22:00:51 +0200

    reducer exercise for reactTS

--- typescript-react-essentials/src/starter/07-reducers/reducer.ts
+++ typescript-react-essentials/src/starter/07-reducers/reducer.ts
@@ -1,13 +1,38 @@
 export const initialState: CounterState = {
   count: 0,
-  status: 'Pending'
-}
+  status: "Pending",
+};
 
 export type CounterState = {
-  count:number;
-  status: string
-}
+  count: number;
+  status: string;
+};
 
-export const counterReducer = (state:CounterState, action:any):CounterState => {
-  return state
-}
+type UpdateCountAction = {
+  type: "increment" | "decrement" | "reset";
+};
+
+type SetStatusAction = {
+  type: "setStatus";
+  payload: "active" | "inactive";
+};
+
+type CounterAction = UpdateCountAction | SetStatusAction;
+
+export const counterReducer = (
+  state: CounterState,
+  action: CounterAction
+): CounterState => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "reset":
+      return { ...state, count: 0 };
+    case "setStatus":
+      return { ...state, status: action.payload };
+    default:
+      return state;
+  }
+};
