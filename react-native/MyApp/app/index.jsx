e83be315a8cd852ff0a371a2e946be4ee9793522
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-04 14:32:53 +0200

    add links to app

--- react-native/MyApp/app/index.jsx
+++ react-native/MyApp/app/index.jsx
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { Link } from 'expo-router'
 import React from 'react'
 import icedCoffeeImg from '@/assets/images/iced-coffee.png'
 
@@ -10,7 +11,8 @@ const app = () => {
         resizeMode='cover'
         style={styles.image}
       >
-        <Text style={styles.text}>Coffee Shop</Text>
+        <Text style={styles.title}>Coffee Shop</Text>
+        <Link href="/explore" style={styles.link}>Explore </Link>
       </ImageBackground>
       
     </View>
@@ -32,11 +34,21 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'center'
   },
-  text: {
+  title: {
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
     textAlign: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    marginBottom: 120
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    textDecorationLine: 'underline',
+    padding: 4
   }
 })
