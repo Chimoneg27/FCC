f22a74acff66f14976957b472f3dc6a1f484943d
Chimoneg27 <chimonegarvin27@gmail.com>
2026-01-15 15:01:28 +0200

    create models and run migrations

--- DjangoFCC/firstproject/firstapp/models.py
+++ DjangoFCC/firstproject/firstapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+  # this is how we define the columns of the table
+  name = models.CharField(max_length=255)
+  price = models.IntegerField()
+  
